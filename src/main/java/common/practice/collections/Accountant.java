785a534102b5cda8d9566030c2cd3d05da8bc38b
ivanskyi <olehfromukraine@gmail.com>
2025-06-02 21:34:51 +0300

    Added "convertToList" method to "Accountant" class

--- src/main/java/common/practice/collections/Accountant.java
+++ src/main/java/common/practice/collections/Accountant.java
@@ -1,11 +1,19 @@
 package common.practice.collections;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Accountant {
 
     public static Integer calculateSum(List<Integer> values) {
         return values.stream()
                 .reduce(0, (a, b) -> a + b);
+    }
+
+    public static List<Integer> convertToList(List<List<Integer>> values) {
+        return values.stream()
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
     }
 }
