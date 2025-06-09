07edd2daab3e901a144b80345d4a8baa6146128b
ivanskyi <olehfromukraine@gmail.com>
2025-06-09 19:49:05 +0300

    Removed "Lombok"

--- src/main/java/common/practice/orm/Employee.java
+++ src/main/java/common/practice/orm/Employee.java
@@ -1,14 +1,8 @@
 package common.practice.orm;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
 import java.math.BigDecimal;
+import java.util.Objects;
 
-@Getter
-@Setter
-@ToString
 @Table(name = "employee")
 public class Employee {
 
@@ -21,4 +15,48 @@ public class Employee {
     private String lastName;
 
     private BigDecimal salary;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(id, employee.id) && Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName) && Objects.equals(salary, employee.salary);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, salary);
+    }
 }
